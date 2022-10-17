3eab28c2eaea6ae9f60b221dde42cecf81e47387
cacau <anaclaudia.sza@outlook.com>
2022-10-17 20:48:59 -0300

    task: revisa req 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,5 @@
 db.produtos.updateMany(
-  { nome: { $ne: "McChicken" } },
-  { $addToSet: { ingredientes: "ketchup" } },
+  { nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: true, ingredientes: true });
