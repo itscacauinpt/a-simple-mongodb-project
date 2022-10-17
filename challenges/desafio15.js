9fbd5fa6cea9542813f2657e5e90853bb8c563c1
cacau <anaclaudia.sza@outlook.com>
2022-10-17 17:52:55 -0300

    feat: avaliacao em todos os documentos da coleção e efetue alterações nesse campo

---
+++ challenges/desafio15.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: 0 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  { $inc: { avaliacao: 3 } },
+);
+
+db.produtos.find({}, { _id: 0, nome: true, avaliacao: true });
